f0f50babdb57d54a17e16d4a70a30c5501c01c49
s40723150 <40723150@gm.nfu.edu.tw>
2020-12-18 16:50:54 +0800

    add corp image codes

--- main.py
+++ main.py
@@ -1,43 +1,39 @@
 import cv2
 import numpy as np
-
-
-def key():
-    key = cv2.waitKey(10) & 0XFF
-    return key
-
-
-def vidshow(show, keys):
-    while show:
-        show, frame = cap.read()
-        cv2.imshow("Video", frame)
-        if key() == keys:
-            break
-
-
-def vidclose(show, keys):
-    while show:
-        if key() == keys:
-            cv2.destroyAllWindows()
-            break
-
-
 cap = cv2.VideoCapture(0)
 esc = 27
+C = ord("c")
 Q = ord("q")
+R = ord("r")
 show, frame = cap.read()
-'''
-f2 = np.array(frame)
 
-cv2.imshow("hight", f2)
-
-clahe = cv2.createCLAHE(clipLimit=2, tileGridSize=(8, 8))
-claheImg = clahe.apply(frame)
-Cimg = cv2.cvtColor(claheImg,cv2.COLOR_GRAY2RGB)
-'''
-# cv2.imshow("Cimg", Cimg)
-# cv2.waitKey(0)
-bbox = cv2.selectROI("img", frame, False, True)
-print(bbox)
-vidclose(show, Q)
-vidshow(show, esc)
+clone = frame.copy()
+points = []
+cropping = False
+def Area(event,x,y,flags,param):
+    global points, cropping
+    if event == cv2.EVENT_LBUTTONDOWN:
+        points = [(x, y)]
+        cropping = True
+    elif event == cv2.EVENT_LBUTTONUP:
+        points.append((x, y))
+        cropping = False
+        cv2.rectangle(frame, points[0], points[1], (0, 255, 0), 2)
+while show:
+    key = cv2.waitKey(5) & 0XFF
+    cv2.setMouseCallback("Video",Area)
+    cv2.imshow("Video", frame)
+    if key == R:
+        points = []
+        frame = clone.copy()
+        cv2.imshow("Video", frame)
+        cv2.destroyWindow("ROI")
+        print("pressed R")
+    elif key == C:
+        print("pressed C")
+        if len(points) == 2:
+            roi = clone[points[0][1]:points[1][1], points[0][0]:points[1][0]]
+            print(roi.shape)
+            cv2.imshow("ROI", roi)
+    elif key == esc:
+        break
