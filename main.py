9bc16f6cd9c5fe42bccf993451c5dde22e32f7a3
s40723150 <40723150@gm.nfu.edu.tw>
2020-12-22 00:31:00 +0800

    test another way to rank dots

--- main.py
+++ main.py
@@ -1,28 +1,25 @@
 import cv2
 import numpy as np
 cap = cv2.VideoCapture(0)
-esc = 27
-C = ord("c")
-Q = ord("q")
-R = ord("r")
-S = ord("s")
+esc, C ,Q , R, S = 27, ord("c"), ord("q"), ord("r"), ord("s")
 show, frame = cap.read()
 TARGET = "TARGET"
 p_c = False
 clone = frame.copy()
 points = []
-cropping = False
+# cropping = False
 def Area(event,x,y,flags,param):
     global points, cropping
     if event == cv2.EVENT_LBUTTONDOWN:
         points = [(x, y)]
-        cropping = True
+        # cropping = True
     elif event == cv2.EVENT_LBUTTONUP:
         points.append((x, y))
-        cropping = False
+        # cropping = False
         cv2.rectangle(frame, points[0], points[1], (0, 255, 0), 2)
 def rank_dot(points):
     global x0, y0, x1, y1
+    # global Point
     x0, y0 = points[0][0], points[0][1]
     x1, y1 = points[1][0], points[1][1]
     if x0 >= x1:
@@ -31,6 +28,8 @@ def rank_dot(points):
             y0, y1 = points[1][1], points[0][1]
     elif y0 >= y1:
         y0, y1 = points[1][1], points[0][1]
+    # Point = [(x0, y0), (x1, y1)]
+    # return Point
 while show:
     key = cv2.waitKey(5) & 0XFF
     cv2.setMouseCallback("Video",Area)
@@ -46,7 +45,7 @@ while show:
         p_c = True
         if len(points) == 2:
             rank_dot(points)
-            # target = clone[points[0][1]:points[1][1], points[0][0]:points[1][0]]
+            # target = clone[Point[0][1]:Point[1][1], Point[0][0]:Point[1][0]]
             target = clone[y0:y1, x0:x1]
             print(target.shape)
             cv2.imshow(TARGET, target)
