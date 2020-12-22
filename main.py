89c00a67fd0a5fbd10876c6a736ba3d702d7ca7c
s40723150 <40723150@gm.nfu.edu.tw>
2020-12-22 22:08:01 +0800

    selecting target has rectangle mark

--- main.py
+++ main.py
@@ -5,21 +5,26 @@ esc, C ,Q , R, S = 27, ord("c"), ord("q"), ord("r"), ord("s")
 show, frame = cap.read()
 TARGET = "TARGET"
 p_c = False
-clone = frame.copy()
+clone = np.copy(frame)
+clone_frame = np.copy(frame)
 points = []
-# cropping = False
+mouse_pressed = False
 def Area(event,x,y,flags,param):
-    global points, cropping
+    global points, mouse_pressed, clone
     if event == cv2.EVENT_LBUTTONDOWN:
+        clone = np.copy(frame)
+        mouse_pressed = True
         points = [(x, y)]
-        # cropping = True
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if mouse_pressed:
+            clone = np.copy(frame)
+            cv2.rectangle(clone, points[0], (x, y), (0, 255, 0), 2)
     elif event == cv2.EVENT_LBUTTONUP:
+        mouse_pressed = False
         points.append((x, y))
-        # cropping = False
-        cv2.rectangle(frame, points[0], points[1], (0, 255, 0), 2)
+        cv2.rectangle(clone, points[0], points[1], (0, 255, 0), 2)
 def rank_dot(points):
     global x0, y0, x1, y1
-    # global Point
     x0, y0 = points[0][0], points[0][1]
     x1, y1 = points[1][0], points[1][1]
     if x0 >= x1:
@@ -28,16 +33,15 @@ def rank_dot(points):
             y0, y1 = points[1][1], points[0][1]
     elif y0 >= y1:
         y0, y1 = points[1][1], points[0][1]
-    # Point = [(x0, y0), (x1, y1)]
-    # return Point
+
 while show:
     key = cv2.waitKey(5) & 0XFF
     cv2.setMouseCallback("Video",Area)
-    cv2.imshow("Video", frame)
+    cv2.imshow("Video", clone)
     if key == R:
         points = []
-        frame = clone.copy()
-        cv2.imshow("Video", frame)
+        clone = np.copy(frame)
+        cv2.imshow("Video", clone)
         cv2.destroyWindow(TARGET)
         p_c = False
         print("pressed R")
@@ -45,8 +49,7 @@ while show:
         p_c = True
         if len(points) == 2:
             rank_dot(points)
-            # target = clone[Point[0][1]:Point[1][1], Point[0][0]:Point[1][0]]
-            target = clone[y0:y1, x0:x1]
+            target = clone_frame[y0:y1, x0:x1]
             print(target.shape)
             cv2.imshow(TARGET, target)
         else:
@@ -61,3 +64,4 @@ while show:
             print("Save the target as image is successful")
     elif key == esc:
         break
+
