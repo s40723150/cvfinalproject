ef0c2c503993563438dccc45e44ac39de9f8436e
s40723150 <40723150@gm.nfu.edu.tw>
2020-12-23 10:55:35 +0800

    when exit select target show the camera frame

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
 import cv2
 import numpy as np
-cap = cv2.VideoCapture(0)
-esc, C ,Q , R, S = 27, ord("c"), ord("q"), ord("r"), ord("s")
-show, frame = cap.read()
-TARGET = "TARGET"
-p_c = False
-clone = np.copy(frame)
-clone_frame = np.copy(frame)
-points = []
-mouse_pressed = False
 def Area(event,x,y,flags,param):
     global points, mouse_pressed, clone
     if event == cv2.EVENT_LBUTTONDOWN:
@@ -33,35 +24,56 @@ def rank_dot(points):
             y0, y1 = points[1][1], points[0][1]
     elif y0 >= y1:
         y0, y1 = points[1][1], points[0][1]
-
-while show:
-    key = cv2.waitKey(5) & 0XFF
-    cv2.setMouseCallback("Video",Area)
-    cv2.imshow("Video", clone)
-    if key == R:
-        points = []
-        clone = np.copy(frame)
+if __name__ == "__main__":
+    cap = cv2.VideoCapture(0)
+    esc, C, Q, R, S = 27, ord("c"), ord("q"), ord("r"), ord("s")
+    show, frame = cap.read()
+    TARGET = "TARGET"
+    p_c = False
+    clone = np.copy(frame)
+    clone_frame = np.copy(frame)
+    points = []
+    mouse_pressed = False
+    show2 = False
+    while show:
+        key = cv2.waitKey(5) & 0XFF
+        cv2.setMouseCallback("Video",Area)
         cv2.imshow("Video", clone)
-        cv2.destroyWindow(TARGET)
-        p_c = False
-        print("pressed R")
-    elif key == C:
-        p_c = True
-        if len(points) == 2:
-            rank_dot(points)
-            target = clone_frame[y0:y1, x0:x1]
-            print(target.shape)
-            cv2.imshow(TARGET, target)
-        else:
-            print("Pleace select target.")
-    elif key == S:
-        if p_c is False:
-            print("Pleace select target.")
-        else:
+        if key == R:
+            points = []
+            clone = np.copy(frame)
+            cv2.imshow("Video", clone)
             cv2.destroyWindow(TARGET)
-            cv2.imshow("target",target)
-            cv2.imwrite("target.png", target, [cv2.IMWRITE_PNG_COMPRESSION, 0])
-            print("Save the target as image is successful")
-    elif key == esc:
-        break
+            p_c = False
+            print("pressed R")
+        elif key == C:
+            p_c = True
+            if len(points) == 2:
+                rank_dot(points)
+                target = clone_frame[y0:y1, x0:x1]
+                print(target.shape)
+                cv2.imshow(TARGET, target)
+            else:
+                print("Pleace select target.")
+        elif key == S:
+            if p_c is False:
+                print("Pleace select target.")
+            else:
+                cv2.destroyWindow(TARGET)
+                cv2.imshow("target",target)
+                cv2.imwrite("target.png", target, [cv2.IMWRITE_PNG_COMPRESSION, 0])
+                print("Save the target as image is successful")
+        elif key == esc:
+            show2 = True
+            cv2.destroyAllWindows()
+            break
+
+    while show2:
+        key = cv2.waitKey(5) & 0XFF
+        show, frame = cap.read()
+        show2 = show
+        cv2.imshow("Video", frame)
+        if key == esc:
+            cv2.destroyAllWindows()
+            break
 
