45aca17a435a987fe060b77f20738637cde057d0
s40723150 <40723150@gm.nfu.edu.tw>
2021-01-02 16:38:23 +0800

    rearrange the track_ball codes

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # cvfinalproject
 
-demo_airhockey_2D.py:利用滑鼠游位置當作球，模擬airhockey的球的移動
+track_ball：主程式
 
-select_target.py:可選取並儲存目標。
+demo_airhockey_2D.py：利用滑鼠游位置當作球，模擬airhockey的球的移動
 
-targettracker:選取目標並追蹤。
+select_target.py：可選取並儲存目標。
+
+targettracker：選取目標並追蹤。
--- code/track_ball.py
+++ code/track_ball.py
@@ -1,6 +1,8 @@
 import cv2, imutils, time
 import numpy as np
 from collections import deque
+
+distance = deque(maxlen=2)
 def bg():
     background = np.ones((600, 300, 3), np.uint8) * 255
     background = cv2.rectangle(background, (0, 0), (299, 300), (0, 0, 0), 3)
@@ -9,14 +11,6 @@ def bg():
     background = cv2.rectangle(background, (100, 560), (200, 600), (0, 0, 0), 5)
     background = cv2.circle(background, (150, 300), 50, (0, 0, 0), 3)
     return background
-'''
-information = np.ones((60, 240, 3), np.uint8) * 255
-information = cv2.rectangle(information, (0,0), (240, 59), (0, 0, 0), 3)
-update = np.copy(background)
-ball_p = []'''
-distance = deque(maxlen=2)
-
-
 def ball(event, x, y, flags, param):
     global update, ball_p
     if event == cv2.EVENT_MOUSEMOVE:
@@ -26,10 +20,8 @@ def Velocity(center , distance = distance ):
     end_t = time.time()
     dt = end_t - start_t
     distance.append(center)
-    # print(distance)
     x0, y0 = distance[0][0]/2, distance[0][1]/2
     x1, y1 = distance[-1][0]/2, distance[-1][1]/2
-
     dx, dy= abs(x0 - x1), abs(y0 - y1)
     ds = (dx ** 2 + dy ** 2) ** 0.5
     velocity = ds / dt
@@ -41,8 +33,6 @@ def position(center):
     color = (205, 205, 80)
     cv2.putText(info, "position", (120, 23), cv2.FONT_HERSHEY_SIMPLEX,  0.6, color, 1)
     cv2.putText(info, "(cm)", (195, 23), cv2.FONT_HERSHEY_SIMPLEX,  0.4, color, 1)
-    # p_str = str(int(center[0]/2))+","+str(int(center[1]/2))
-    # cv2.putText(info, p_str, (120, 45), cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 1)
     px_str = "x:"+ str(int(center[0]/2))
     py_str = "y:"+ str(int(center[1]/2))
     cv2.putText(info, px_str, (120, 45), cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 1)
